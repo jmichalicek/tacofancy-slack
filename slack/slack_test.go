51e9e5de93da923e93f1268be7853b8d2f1ecc87
Justin Michalicek <jmichailcek@gmail.com>
2017-10-08 00:09:08 -0400

    a few skeleton tests which do nothing

---
+++ slack/slack_test.go
@@ -0,0 +1,19 @@
+package slack
+
+import "testing"
+
+func TestSendDelayedResponse(t *testing.T) {
+    // TODO
+}
+
+func TestSlashCommandBuildResponse(t *testing.T) {
+        // TODO
+}
+
+func TestSlashCommandRespondAsync(t *testing.T) {
+    // TODO
+}
+
+func TestVerifyToken(t *testing.T) {
+    // TODO
+}
