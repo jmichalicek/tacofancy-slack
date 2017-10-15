d0f68ae5f4ad2bc957df128aa36fe39ca1ab753e
Justin Michalicek <jmichalicek@gmail.com>
2017-10-15 19:27:17 -0400

    gofmt

--- tacocli/tacocli.go
+++ tacocli/tacocli.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "github.com/jmichalicek/tacofancy-slack/tacofancy"
-    "fmt"
-    "encoding/json"
-    "flag"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/jmichalicek/tacofancy-slack/tacofancy"
 )
 
 func showTaco(taco tacofancy.Taco, showJSON bool, showDesc bool) {
-    s, err := json.MarshalIndent(taco, "", "  ")
-    if err != nil {
-        fmt.Println(err)
-    }
+	s, err := json.MarshalIndent(taco, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    if showJSON {
-        fmt.Println(string(s))
-    }
+	if showJSON {
+		fmt.Println(string(s))
+	}
 
-    if showDesc {
-        fmt.Println(taco.Description())
-    }
+	if showDesc {
+		fmt.Println(taco.Description())
+	}
 }
 
 func main() {
-    showDesc := flag.Bool("description", false, "Show the description output")
-    showJSON := flag.Bool("json", false, "Show the JSON output")
-    randomTaco := flag.Bool(
-        "random", false, "Show a random selection of base layer, seasoning, mixin, condiment, and shell")
-    flag.Parse()
+	showDesc := flag.Bool("description", false, "Show the description output")
+	showJSON := flag.Bool("json", false, "Show the JSON output")
+	randomTaco := flag.Bool(
+		"random", false, "Show a random selection of base layer, seasoning, mixin, condiment, and shell")
+	flag.Parse()
 
-		// default url and http.Client
-    client := tacofancy.NewClient("", nil)
-    if *randomTaco {
-        // showRandomTacoParts(*showJSON, *showDesc)
-        taco, err := client.GetRandomTacoParts()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        showTaco(&taco, *showJSON, *showDesc)
-    } else {
-        //showFullRandomTaco(*showJSON, *showDesc)
-        taco, err := client.GetRandomFullTaco()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        showTaco(&taco, *showJSON, *showDesc)
-    }
+	// default url and http.Client
+	client := tacofancy.NewClient("", nil)
+	if *randomTaco {
+		taco, err := client.GetRandomTacoParts()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		showTaco(&taco, *showJSON, *showDesc)
+	} else {
+		taco, err := client.GetRandomFullTaco()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// ideally no pointer here, but the unmarshall seems to need it.
+		// other option is type switch stuff in showTaco
+		showTaco(&taco, *showJSON, *showDesc)
+	}
 }
