4fbe59ea69ed01a7a9083b619ddd6db782dc85fa
Justin Michalicek <jmichalicek@gmail.com>
2017-10-15 19:32:11 -0400

    skeleton tests for tacofancy/types.go

---
+++ tacofancy/types_test.go
@@ -0,0 +1,45 @@
+package tacofancy
+
+import "testing"
+
+
+func TestNewBaseTaco(t *testing.T) {
+}
+
+func TestNewRandomTaco(t *testing.T) {
+
+}
+
+func TestNewFullTaco(t *testing.T) {
+
+}
+
+func TestBaseTacoDescription(t *testing.T) {
+
+}
+
+func TestRandomTacoDescription(t *testing.T) {
+
+}
+
+func TestRandomTacoUnmarshalJSON(t *testing.T) {
+
+}
+
+func TestRandomTacoMarshalJSON(t *testing.T) {
+
+}
+
+func TestFullTacoDescription(t *testing.T) {
+
+}
+
+func TestFullTacoUnmarshalJSON(t *testing.T) {
+
+}
+
+func TestFullTacoMarshalJSON(t *testing.T) {
+
+}
+
+// TODO: test other getters?  Or just test that interface is satisfied for various tacos?
